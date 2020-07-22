2ff100a6c2e7cddc7219a60f5e467c716613fcec
unknown <ponggas2@gmail.com>
2020-07-22 17:56:03 +0900

    배열 추가 삭제

--- bitc_react/second-app/src/App.js
+++ bitc_react/second-app/src/App.js
@@ -1,24 +1,59 @@
 import React, { Component } from "react";
+import Post from "./Post";
+import styled from "styled-components";
 
-// ref 는 돔에 직접적으로 접근 할 수 있다
 class App extends Component {
-  changeFocus = () => {
-    this.input.focus();
+  state = {
+    posts: [
+      {
+        id: 1,
+        title: "제목1",
+      },
+      {
+        id: 2,
+        title: "제목2",
+      },
+      {
+        id: 3,
+        title: "제목3",
+      },
+    ],
   };
 
   render() {
+    const ContainerBox = styled.div`
+      display: grid;
+      justify-content: center;
+    `;
+
+    const add = () => {
+      // render안에 const로 만들면 계속 초기화 되지않는다
+      this.setState({
+        posts: this.state.posts.concat({
+          id: 4,
+          title: "제목4",
+        }),
+      });
+    };
+
+    const del = () => {
+      // render안에 const로 만들면 계속 초기화 되지않는다
+      this.setState({
+        posts: this.state.posts.filter((post) => {
+          return post.id !== 2;
+        }),
+      });
+    };
+
     return (
-      <div>
-        <input
-          ref={(ref) => {
-            this.input = ref;
-          }}
-          type="text"
-          placeholder="Username"
-        />
-        <input type="text" placeholder="Password" />
-        <button onClick={this.changeFocus}>포커스이동</button>
-      </div>
+      <ContainerBox>
+        <button onClick={add}>추가</button>
+        <button onClick={del}>삭제</button>
+
+        {this.state.posts.map((post) => {
+          return <Post id={post.id} title={post.title} />;
+        })}
+      </ContainerBox>
     );
   }
 }

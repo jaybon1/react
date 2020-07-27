ec8df941e87c5aaa4b2ce8e497700edb098b1320
unknown <ponggas2@gmail.com>
2020-07-27 17:32:48 +0900

    link to 연습

--- bitc_react/second-app/src/App.js
+++ bitc_react/second-app/src/App.js
@@ -1,72 +1,23 @@
 import React, { Component } from "react";
-import Post from "./Post";
-import styled from "styled-components";
+import Home from "./page/Home";
+import About from "./page/About";
+import Profile from "./page/Profile";
+import Nav from "./components/Nav";
+import { Route } from "react-router-dom";
 
-const ContainerBox = styled.div`
-  display: grid;
-  justify-content: center;
-`;
-
+// exact={true} 는 정확히 주소가 맞아야먄 나오도록 할 수 있다.
 class App extends Component {
-  state = {
-    posts: [
-      {
-        id: 1,
-        title: "제목1",
-      },
-      {
-        id: 2,
-        title: "제목2",
-      },
-      {
-        id: 3,
-        title: "제목3",
-      },
-    ],
-  };
-
   render() {
-    const add = () => {
-      // render안에 const로 만들면 계속 초기화 되지않는다
-      this.setState({
-        posts: this.state.posts.concat({
-          id: 4,
-          title: "제목4",
-        }),
-      });
-    };
-
-    const del = () => {
-      // render안에 const로 만들면 계속 초기화 되지않는다
-      this.setState({
-        posts: this.state.posts.filter((post) => {
-          return post.id !== 2;
-        }),
-      });
-    };
-
-    const update = () => {
-      const data = {
-        id: 2,
-        title: "제목200",
-      };
-
-      this.setState({
-        posts: this.state.posts.map((post) =>
-          post.id === 2 ? { ...post, ...data } : post
-        ),
-      });
-    };
-
     return (
-      <ContainerBox>
-        <button onClick={add}> 추가 </button>
-        <button onClick={del}> 삭제 </button>
-        <button onClick={update}> 수정 </button>
-        {this.state.posts.map((post) => {
-          return <Post id={post.id} title={post.title} />;
-        })}{" "}
-      </ContainerBox>
+      <div>
+        <Nav />
+        <hr />
+        <Route path="/" component={Home} exact={true} />
+        <hr />
+        <Route path="/about" component={About} />
+        <hr />
+        <Route path="/profile" component={Profile} />
+      </div>
     );
   }
 }
